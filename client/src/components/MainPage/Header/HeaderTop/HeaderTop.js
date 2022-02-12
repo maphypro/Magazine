f70e0198f1e00b4396305998007d417a2fe6a91f
Slava Vesnin <maphypro@gmail.com>
2022-02-12 22:42:03 +0700

    Fix top header

---
+++ client/src/components/MainPage/Header/HeaderTop/HeaderTop.js
@@ -0,0 +1,14 @@
+import s from './HeaderTop.module.scss'
+
+const HeaderTop = () => {
+    return (
+        <div className={s.HeaderTopWrapper}>
+            <div className={s.HeaderTop}>
+                <p className={s.mainText}>Скидка 10% по промокоду “ZAMOK” на все заказы до 10.09</p>
+                <p className={s.sideText}>Обратный звонок</p>
+            </div>
+        </div>
+    )
+}
+
+export default HeaderTop;
