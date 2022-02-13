e27c1c6b88c93dee29887dca328d43b8e4a0b1fc
Slava Vesnin <maphypro@gmail.com>
2022-02-13 11:07:37 +0700

    Add a number of goods indicator

--- client/src/components/MainPage/Header/HeaderBottom/FavoritesAndCardWrapper/Card/Card.js
+++ client/src/components/MainPage/Header/HeaderBottom/FavoritesAndCardWrapper/Card/Card.js
@@ -3,7 +3,10 @@ import card_icon from '../../../../../../assets/icons/card.svg'
 
 const Card = () => {
     return (
-        <img className={s.card_icon} src={card_icon} alt="card_icon"/>
+        <div className={s.card}>
+            <img className={s.card_icon} src={card_icon} alt="card_icon"/>
+            <div className={s.amount_circle}>4</div>
+        </div>
     )
 }
 
--- client/src/components/MainPage/Header/HeaderBottom/FavoritesAndCardWrapper/Favorites/Favorites.js
+++ client/src/components/MainPage/Header/HeaderBottom/FavoritesAndCardWrapper/Favorites/Favorites.js
@@ -3,7 +3,10 @@ import favorites_icon from '../../../../../../assets/icons/heart.svg'
 
 const Favorites = () => {
     return (
-        <img className={s.favorites_icon} src={favorites_icon} alt="favorites_icon" />
+        <div className={s.favorites}>
+            <img className={s.favorites_icon} src={favorites_icon} alt="favorites_icon"/>
+            <div className={s.amount_circle}>1</div>
+        </div>
     )
 }
 
